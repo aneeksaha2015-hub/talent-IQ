ff6413a9a18234cbc51bb9b43ee7d4ae2c382640
Aneek <aneeksaha2015@gmail.com>
2026-01-19 01:32:33 +0530

    validation check added for DB_URL

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -5,7 +5,9 @@ export const connectDB = async () => {
     try {
         // Debug check: This will print in your console so you can see if the URL is correct
         console.log("Attempting to connect with URL:", ENV.DB_URL);
-
+        if(!ENV.DB_URL) {
+            throw new Error("DB_URL is not defined in enviornment variables!");
+        }
         const conn = await mongoose.connect(ENV.DB_URL, {
             serverSelectionTimeoutMS: 5000,
             family: 4, // Forces IPv4 (your successful test used IPv4)
