a0db797d2808252ad76dd8ede29dbecf7bc00b43
Aneek <aneeksaha2015@gmail.com>
2026-01-21 16:19:15 +0530

    auth and db sync

---
+++ backend/src/lib/inngest.js
@@ -0,0 +1,35 @@
+import {Inggest} from "inngest"
+import { connectDB } from "./db"
+import User from "../models/User"
+
+export const inngest = new Inngest({ id: "talent-iq" });
+
+const syncUser = inngest.createFunction(
+    {id:"sync-user"},
+    {event:"clerk/user.created"},
+    async ({event}) => {
+        await connectDB();
+        const {id,email,first_name,last_name,image_url} = event.data;
+
+        const newUser = {
+            clerkId:id,
+            email:email_addresses[0]?.email_address,
+            name: `${first_name || ""} ${last_name || ""}`,
+            profileImage:image_url
+        }
+
+        await User.create(newUser);
+    }
+)
+
+const deleteUserFromDB = inngest.createFunction(
+    {id:"deleted-user-from-db"},
+    {event:"clerk/user.deleted"},
+    async ({event}) => {
+        await connectDB();
+        const {id} = event.data;
+        await User.deleteOne({clerkId:id});
+    }
+)
+
+export const functions = [syncUser,deleteUserFromDB];
