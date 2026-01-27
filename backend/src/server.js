cd30d027ae6ecce9e9fbbaa22b5ae20ff16b9125
Aneek <aneeksaha2015@gmail.com>
2026-01-27 12:31:51 +0530

    server side auth implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,13 @@ import express from "express";
 import path from "path";
 import cors from "cors";
 import {serve} from "inngest/express";
+import { clerkMiddleware } from '@clerk/express'
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
 import { inngest } from "./lib/inngest.js";
 import { functions } from "./lib/inngest.js";
+import chatRoutes from "./routes/chatRoutes.js"
+
 
 const app = express();
 
@@ -13,14 +16,14 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cors({origin:ENV.CLIENT_URL,credentials:true }));
+app.use(clerkMiddleware());
 
 app.use("/api/inngest", serve({client: inngest, functions}));
+app.use("/api/chat", chatRoutes);
 
 app.get("/health", (req, res) => {
+  req.auth;
   res.status(200).json({ msg: "Api is up and running" });
-});
-app.get("/books", (req, res) => {
-  res.status(200).json({ msg: "this is the books endpoint" });
 });
 
 if (ENV.NODE_ENV == "production") {  
