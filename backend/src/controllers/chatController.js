a1440b97be7cb5d62a67db825cd1b6a7c9a633fe
Aneek <aneeksaha2015@gmail.com>
2026-01-27 12:50:25 +0530

    .. problem fix

--- backend/src/controllers/chatController.js
+++ backend/src/controllers/chatController.js
@@ -1,4 +1,4 @@
-import { chatClient } from ".../lib/stream.js";
+import { chatClient } from "../lib/stream.js";
 export async function getStreamToken(req,res) {
     try{
         const token=chatClient.createToken(req.user.clerkId);
